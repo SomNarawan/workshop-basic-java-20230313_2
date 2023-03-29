078903ffe3619efdb1ea4905f169416c7bb8b75d
narawan.i <narawan.i@kbtg.tech>
2023-03-29 22:19:46 +0700

    Add description condition

--- hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
@@ -20,3 +20,11 @@ public class FizzBuzz {
         return String.valueOf(input);
     }
 }
+
+/**
+ * Condition 1 :: 3,5 => FizzBuzz
+ * Condition 2 :: 3 => Fizz
+ * Condition 3 :: 5 => Buzz
+ * Condition 4 :: Same number
+ * Condition 5 :: 7 => KBTG BASIC JAVA
+ */
