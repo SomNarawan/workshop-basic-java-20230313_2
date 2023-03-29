75c65ae2f7f34c0e080e8c7bf3231f20b1bfa2fa
narawan.i <narawan.i@kbtg.tech>
2023-03-29 21:03:18 +0700

    FizzBuzz Case01

--- hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
@@ -1,4 +1,7 @@
 package com.example.hellospring.workshop01;
 
 public class FizzBuzz {
+    public Integer fizzBuzz(Integer input){
+        return input;
+    }
 }
--- hello-spring/src/test/java/com/example/hellospring/workshop01/FizzBuzzTest.java
+++ hello-spring/src/test/java/com/example/hellospring/workshop01/FizzBuzzTest.java
@@ -1,7 +1,21 @@
 package com.example.hellospring.workshop01;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
+    @Test
+    @DisplayName("input = 1")
+    public void case01() {
+        Integer input = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Integer expectedResult = 1;
+        Integer actualResult = fizzBuzz.fizzBuzz(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
