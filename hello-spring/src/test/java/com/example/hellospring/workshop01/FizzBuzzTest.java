8356142ace0fe800b99ab3d1e4d4a216523c08b1
narawan.i <narawan.i@kbtg.tech>
2023-03-29 21:05:52 +0700

    FizzBuzz case2,4,7,8,11

--- hello-spring/src/test/java/com/example/hellospring/workshop01/FizzBuzzTest.java
+++ hello-spring/src/test/java/com/example/hellospring/workshop01/FizzBuzzTest.java
@@ -18,4 +18,64 @@ class FizzBuzzTest {
 
         assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    @DisplayName("input = 2")
+    public void case02() {
+        Integer input = 2;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Integer expectedResult = 2;
+        Integer actualResult = fizzBuzz.fizzBuzz(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("input = 4")
+    public void case04() {
+        Integer input = 4;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Integer expectedResult = 4;
+        Integer actualResult = fizzBuzz.fizzBuzz(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("input = 7")
+    public void case07() {
+        Integer input = 7;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Integer expectedResult = 7;
+        Integer actualResult = fizzBuzz.fizzBuzz(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("input = 8")
+    public void case08() {
+        Integer input = 8;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Integer expectedResult = 8;
+        Integer actualResult = fizzBuzz.fizzBuzz(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("input = 11")
+    public void case11() {
+        Integer input = 11;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Integer expectedResult = 11;
+        Integer actualResult = fizzBuzz.fizzBuzz(input);
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
