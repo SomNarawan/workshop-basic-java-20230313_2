592a60d42a0f52ddaf9089941741830e5d34b7f0
narawan.i <narawan.i@kbtg.tech>
2023-03-29 22:00:22 +0700

    Move logic of fizzbuzz rult to new class

--- hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzz.java
@@ -2,16 +2,12 @@ package com.example.hellospring.workshop01;
 
 public class FizzBuzz {
     public String fizzBuzz(Integer input){
-        String output = "";
-        if(input%3 == 0){
-            output = output.concat("Fizz");
+        FizzBuzzCondition fizzBuzzCondition = new FizzBuzzCondition();
+        if(fizzBuzzCondition.check(input)){
+            return fizzBuzzCondition.print();
         }
-        if(input%5 == 0){
-            output = output.concat("Buzz");
-        }
-        if(!output.contains("Fizz") && !output.contains("Buzz")){
-            output = String.valueOf(input);
-        }
-        return output;
+        if(input%3 == 0) return "Fizz";
+        if(input%5 == 0) return "Buzz";
+        return String.valueOf(input);
     }
 }
---
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/FizzBuzzCondition.java
@@ -0,0 +1,11 @@
+package com.example.hellospring.workshop01;
+
+public class FizzBuzzCondition {
+    public boolean check(Integer input) {
+        return input % 3 == 0 & input % 5 == 0;
+    }
+
+    public String print(){
+        return "FizzBuzz";
+    }
+}
