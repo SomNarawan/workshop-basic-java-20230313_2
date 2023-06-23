dd10818b782882402cd1af8f82fd9af145fdc2fb
narawan.i <narawan.i@kbtg.tech>
2023-06-23 15:32:06 +0700

    Create first controller

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -1,12 +1,14 @@
 package com.example.hellorest.employee;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+    @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeById(int id) {
-    return new EmployeeResponse(id, "Somkiat", "Pui");
+        return new EmployeeResponse(id, "Somkiat", "Pui");
     }
 
 }
