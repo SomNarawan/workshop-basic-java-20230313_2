10739b87fe5f4c1cf13e9a473bb519630a8275bc
narawan.i <narawan.i@kbtg.tech>
2023-06-26 09:28:32 +0700

    Working with request parameters

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,4 +21,17 @@ public class EmployeeController {
         return new EmployeeResponse(_id, "Somkiat", "Pui");
     }
 
+    // employee?id=?
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeById2(@RequestParam String id) {
+        // Validate id => Number Only
+        int _id = 0;
+        try {
+            _id = Integer.parseInt(id);
+        }catch (NumberFormatException e){
+            // ERROR => TODO ?
+
+        }
+        return new EmployeeResponse(_id, "Somkiat", "Pui");
+    }
 }
